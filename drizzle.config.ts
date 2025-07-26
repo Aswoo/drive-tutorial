51b35304ec79c2257cf508bbbfdda24c1ed2fd20
Aswoo <yhnu191919@gmail.com>
2025-07-26 15:30:45 +0900

    ci and singlestore db setting

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,19 @@
 import { type Config } from "drizzle-kit";
+import dotenv from "dotenv";
+import fs from "fs";
 
-import { env } from "~/env";
+dotenv.config(); // .env 파일을 직접 읽어서 process.env에 주입
 
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
+  dialect: "singlestore",
+  tablesFilter: ["drive_tutorial_*"],
   dbCredentials: {
-    url: env.DATABASE_URL,
+    host: process.env.SINGLESTORE_HOST!,
+    port: parseInt(process.env.SINGLESTORE_PORT!),
+    user: process.env.SINGLESTORE_USER!,
+    password: process.env.SINGLESTORE_PASS!,
+    database: process.env.SINGLESTORE_DB_NAME!,
+    ssl: {},
   },
-  tablesFilter: ["drive-tutorial_*"],
-} satisfies Config;
+} as Config;
